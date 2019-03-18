3e86ef24ae06e5a5fa6851f89e061fb31814193d
Daehyun Kim <sanote@msn.com>
2019-03-19 02:08:30 +0900

    웹툰 추가. 테마 이미지 변경. 빠진 카드 추가. 트위터, SNS 링크를 위한 설명 추가. js, css 최적화. 카드 내 웹툰 테두리 변경.

---
+++ js/global.min.ed6d9579f10ed83b621f1d91ecb418750e4f069b4f950aeb5f5540f5315e99d2.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(event){var menu=document.querySelector('.top-bar-left');document.querySelector('.site-hamburger').onclick=function(){console.log(menu.style.display);if(menu.style.display=='block'){menu.style.display='none';}else{menu.style.display='block';}}
+window.addEventListener('resize',function(){winWidth=window.innerWidth||document.documentElement.clientWidth;if(winWidth>=768&&menu.style.display=='none')menu.style.display='block';else if(winWidth<768&&menu.style.display=='block')menu.style.display='none';});});

ec188be4047fa12ed22c3288e82e114fc8eaf3c9
Kim Daehyun <sanote@msn.com>
2019-03-22 13:08:38 +0900

    사이트 메뉴 추가.

---
+++ js/bundle.min.54b97c0dacbde82b1b6c564b8cedbbd2642d062bc3a606a4d074cbd32f12d9a9.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded",function(){var winWidth=window.innerWidth||document.documentElement.clientWidth;var menu=document.querySelector('aside');menu.querySelector('.top-bar .close').onclick=function(){menu.style.display="none";}
+document.querySelector('.top-bar .menu').onclick=function(){menu.style.display='block';}
+window.addEventListener('resize',function(){resizeThumb();});resizeThumb();function resizeThumb(){var lists=document.querySelectorAll(".list .thumb .scroll");for(var i=0;i<lists.length;i++){function initImg(box){var to=setInterval(function(){box.style.display="block";box.scrollLeft=(600-box.clientWidth)/2;var w=box.querySelector('img').naturalWidth;box.querySelector('img').width=w/2;if(w>0)clearInterval(to);},100);}
+initImg(lists[i]);}}});

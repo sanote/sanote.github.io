e4bbdaf3f99b717486e1f8fe910cd6edfffd98d2
Daehyun Kim <sanote@msn.com>
2019-03-24 00:55:01 +0900

    권민호 2018 경남도지사 공약 추가. 카드 오타 수정. 홈 화면 댓글 기능 추가. 모바일에서 검색 선택시 스크롤 적용.

---
+++ js/bundle.min.0b8cc416116f5afd886188aee7abb60da4370bba1d85e5fc48bf16b7e2c73242.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded",function(){var winWidth=window.innerWidth||document.documentElement.clientWidth;var menu=document.querySelector('aside');var topbar={btnMenu:document.querySelector('.site-hamburger'),showMenu:winWidth>=768}
+menu.querySelector('.close').onclick=function(){menu.style.display="none";}
+document.querySelector('.top-bar .menu').onclick=function(){menu.style.display='block';}
+if(document.querySelector('section.home #search-by')){document.querySelector('section.home #search-by').onclick=function(){if(winWidth<768.98)window.scrollTo(0,this.offsetTop-10);}}
+window.addEventListener('resize',function(){winWidth=window.innerWidth||document.documentElement.clientWidth;if(!topbar.showMenu&&winWidth>=768){menu.style.display='table-cell';topbar.showMenu=true;}
+else if(topbar.showMenu&&winWidth<768.98){menu.style.display='none';topbar.showMenu=false;}
+resizeThumb();});resizeThumb();function resizeThumb(){var lists=document.querySelectorAll(".list .thumb .scroll");for(var i=0;i<lists.length;i++){function initImg(box){var to=setInterval(function(){box.style.display="block";box.scrollLeft=(600-box.clientWidth)/2;var w=box.querySelector('img').naturalWidth;box.querySelector('img').width=w/2;if(w>0)clearInterval(to);},100);}
+initImg(lists[i]);}}});
